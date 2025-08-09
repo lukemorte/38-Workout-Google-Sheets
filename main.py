3524d4c48ca02513cb2744c1ce45ce83b3423df8
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-09 21:23:27 +0200

    Initialize main.py with API configuration and environment variable loading

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # 38 - Workout Google Sheets
 
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = os.getenv("NUTRITION_API_ID")
+API_KEY = os.getenv("NUTRITION_API_KEY")
