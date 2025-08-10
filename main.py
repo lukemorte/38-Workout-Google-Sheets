2e5f6f82766af628974cd5e34b2b5caaf1f262b6
Jakub Lukáš <jakublukas.eclairdesign@gmail.com>
2025-08-10 05:44:19 +0200

    Add test data for exercise tracking and implement sheet request functionality

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import requests
 import os
 from dotenv import load_dotenv
+from test_data import test_data
+from datetime import datetime
 
 load_dotenv()
 
@@ -17,17 +19,46 @@ HTTP_HEADER = {
     "x-remote-user-id": "0",
 }
 
-request_data = {
-    "query": input("What was your excercise?: "),
-    "gender": "male",
-    "weight_kg": 94,
-    "height_cm": 173,
-    "age": 47,
-}
+# request_data = {
+#     "query": input("Tell me which excercise you did?: "),
+#     "gender": "male",
+#     "weight_kg": 94,
+#     "height_cm": 173,
+#     "age": 47,
+# }
 
 request_url = API_URL + "/v2/natural/exercise"
 
-response = requests.post(url=request_url, data=request_data, headers=HTTP_HEADER)
+# response = requests.post(url=request_url, data=request_data, headers=HTTP_HEADER)
+# print(response.status_code)
+# print(response.json())
 
-print(response.status_code)
-print(response.json())
+now_formatted = datetime.now().strftime("%d/%m/%Y")
+now_time_formatted = datetime.now().strftime("%H:%M")
+
+
+# print(now_formatted)
+
+for item in test_data["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": now_formatted,
+            "time": now_time_formatted,
+            "excercise": item["name"].title(),
+            "duration": item["duration_min"],
+            "calories": item["nf_calories"],
+        }
+    }
+
+    x_header = {
+        "Authorization": "Basic eHVzZXI6ZGVmeDE0",
+        "Content-Type": "application/json",
+    }
+
+    print(sheet_inputs)
+
+    BASIC_USERNAME = "xuser"
+    BASIC_PASSWORD = "defx14"
+    sheet_request_url = "https://api.sheety.co/5f2e71425f09ba9a917864d228b52ef4/myWorkouts/workouts"
+    response = requests.post(url=sheet_request_url, json=sheet_inputs, headers=x_header, auth=(BASIC_USERNAME, BASIC_PASSWORD))
+    print(response.status_code)
---
+++ test_data.py
@@ -0,0 +1,36 @@
+test_data = {
+    'exercises': [
+        {
+            'tag_id': 317,
+            'user_input': 'run',
+            'duration_min': 31.08,
+            'met': 9.8,
+            'nf_calories': 477.18,
+            'photo': {
+                'highres': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_highres.jpg',
+                'thumb': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/317_thumb.jpg',
+                'is_user_uploaded': False
+            },
+            'compendium_code': 12050,
+            'name': 'running',
+            'description': None,
+            'benefits': None
+        },
+        {
+            'tag_id': 5,
+            'user_input': 'cycled',
+            'duration_min': 20,
+            'met': 10,
+            'nf_calories': 313.33,
+            'photo': {
+                'highres': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/5_highres.jpg',
+                'thumb': 'https://d2xdmhkmkbyw75.cloudfront.net/exercise/5_thumb.jpg',
+                'is_user_uploaded': False
+            },
+            'compendium_code': 1040,
+            'name': 'road cycling',
+            'description': None,
+            'benefits': None
+        }
+    ]
+}
